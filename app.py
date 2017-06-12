050b4ec23926f18eec7e62504e1b9b240a829f74
JohnVonNeumann <louiswillcock@gmail.com>
2017-06-12 16:28:59 +1000

    wrestled with this one for a bit, couldnt work out why i couldnt output to the screen, print worked to the console, but return would provide a 500 error, found out that flask requires jsonify to return json to the client

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 from poloniex import Poloniex
 from flask import Flask
+from flask import jsonify
 polo = Poloniex()
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return lbcAsks() 
-
-def orderBook():
+def post():
+  polo = Poloniex()
   lbc_asks = polo.returnOrderBook('BTC_LBC')['asks']
-  lbc_bids = polo.returnOrderBook('BTC_LBC')['bids']
+  return jsonify(lbc_asks)
+  for each in lbc_asks:
+    return('each')  
 
-def lbcAsks():
-  print('LBC ASKS:')
-  for each in orderBook.lbc_asks:
-    print(each)
+#def lbcAsks():
+#  lbc_asks = polo.returnOrderBook('BTC_LBC')['asks']
+#  return('LBC ASKS:')
+#  for each in lbc_asks:
+#    return each
 
-def lbcBids():
-  print('LBC BIDS:')
-  for each in orderBook.lbc_bids:
-    print(each)
+#def lbcBids():
+#  lbc_bids = polo.returnOrderBook('BTC_LBC')['bids']
+#  print('LBC BIDS:')
+#  for each in lbc_bids:
+#    print(each)
 
 if __name__ == "__main__":
   app.run()
