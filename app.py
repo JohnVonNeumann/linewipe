398641a2520d71fde21f2ffce925fa2cb3b1633c
JohnVonNeumann <louiswillcock@gmail.com>
2017-09-11 22:49:50 +1000

    commit implements a simple request cache, was getting stuck too often due to API call limits, creates a list of all ticker names as intended (slight lie, thought id have to do some dict nest traversal), done for tonight, nightynight

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from urllib import request
-import requests
+import sqlite3
 
-response_raw =  requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum/')
+import requests
+import requests_cache
+
+requests_cache.install_cache('coinmarketcap_cache', \
+                             backend='sqlite', \
+                             expire_after=300)
+
+response_raw =  requests.get('https://api.coinmarketcap.com/v1/ticker/')
 headers = response_raw.headers
 response = response_raw.json()
 #import ipdb; ipdb.set_trace()
-print(headers)
+#print(response)
 
 tickers = []
+ticker_count = 0
 for each in response:
     print("This is the name of the asset: " + each['name'])
+    tickers.append(each['name'])
+    ticker_count += 1
 
-print("This is the name of the asset: " + response[0]["name"])
