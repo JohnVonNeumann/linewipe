70b04a9b81d275c7f2e6590094fca6c9612f6ecb
JohnVonNeumann <louiswillcock@gmail.com>
2017-09-11 20:51:39 +1000

    an update to the project and purpose of it, README includes high-level not-pseudocode-pseudocode, if you get what I mean, I think I've learnt a fair bit since last trying to do this, no GUI for this one

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-import json
-from poloniex import Poloniex
-from flask import Flask, jsonify, Response
-polo = Poloniex()
-app = Flask(__name__)
+from urllib import request
+import requests
 
-@app.route("/")
-def post():
-  lbc_asks = polo.returnOrderBook('BTC_LBC')['asks']
-  json_asks = jsonify(lbc_asks)
-  for each in json_asks:  
-    return Response(json.dumps(each), mimetype='application/json') 
+response_raw =  requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
+#headers = response.info()
+response = response_raw.json()
+#import ipdb; ipdb.set_trace()
+print(response)
 
-def lbcAsks(self):
-  lbc_asks = polo.returnOrderBook('BTC_LBC')['asks']
-  for each in lbc_asks:  
-    return jsonify(each)
+#tickers = []
+#for name in response:
+#    print("This is the name of the asset: " + name)
+#    print(name['0']['name'])
 
-def lbcBids(self):
-  lbc_bids = polo.returnOrderBook('BTC_LBC')['bids']
-  return('LBC BIDS:')
-  for each in lbc_bids:
-    return jsonify(each)
-
-if __name__ == "__main__":
-  app.run(debug = True, port = 5000)
+print("This is the name of the asset: " + response[0]["name"])
+print(response)
